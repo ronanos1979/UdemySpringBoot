2acb3d012d5130d66afe59910cd3bb558ca992ea
O'Sullivan, Ronan <Ronan.OSullivan@verint.com>
2024-02-26 12:40:58 -0600

    Initial Draft of Intro To Spring Framework

---
+++ introToSpringFramework/src/main/java/com/ronanos/spring/introToSpringFramework/IntroToSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.ronanos.spring.introToSpringFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ introToSpringFramework/src/test/java/com/ronanos/spring/introToSpringFramework/IntroToSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronanos.spring.introToSpringFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
